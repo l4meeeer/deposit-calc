5967c8647dc2d788eb234ba750a60c1b6e8cef43
l4meeeer <ilya98i13@gmail.com>
2017-05-21 22:52:00 +0700

    NewFix

--- test/deposit_test.c
+++ test/deposit_test.c
@@ -6,12 +6,12 @@
 
 CTEST(check_data, test1)
 {
-	ASSERT_EQUAL(0, data(10000, 365));
+	ASSERT_EQUAL(0, data(10000, 365)); // что хотим получить, что вводим ret
 }
 
 CTEST(check_data, test2)
 {
-        ASSERT_EQUAL(-1, data(9999, 365));
+        ASSERT_EQUAL(-1, data(9999, 365));// мин.сум вклада
 }
 
 CTEST(check_data, test3)
@@ -36,57 +36,57 @@ CTEST(check_data, test6)
 
 //Tests for results
 
-CTEST(calculating, test1)
+CTEST(calculate, test1)
 {
-        ASSERT_EQUAL(90000, result(100000, 30));
+        ASSERT_EQUAL(90000, result(100000, 30)); //
 } 
 
-CTEST(calculating, test2)
+CTEST(calculate, test2)
 {
         ASSERT_EQUAL(102000, result(100000, 31));
 }
 
-CTEST(calculating, test3)
+CTEST(calculate, test3)
 {
         ASSERT_EQUAL(102000, result(100000, 120));
 }
 
-CTEST(calculating, test4)
+CTEST(calculate, test4)
 {
         ASSERT_EQUAL(106000, result(100000, 121));
 }
 
-CTEST(calculating, test5)
+CTEST(calculate, test5)
 {
         ASSERT_EQUAL(106000, result(100000, 240));
 }
 
-CTEST(calculating, test6)
+CTEST(calculate, test6)
 {
         ASSERT_EQUAL(112000, result(100000, 241));
 }
 
-CTEST(calculating, test7)
+CTEST(calculate, test7)
 {
         ASSERT_EQUAL(99000, result(110000, 30));
 } 
 
-CTEST(calculating, test8)
+CTEST(calculate, test8)
 {
         ASSERT_EQUAL(113300, result(110000, 31));
 }
 
-CTEST(calculating, test9)
+CTEST(calculate, test9)
 {
         ASSERT_EQUAL(113300, result(110000, 120));
 }
 
-CTEST(calculating, test10)
+CTEST(calculate, test10)
 {
         ASSERT_EQUAL(118800, result(110000, 121));
 }
 
-CTEST(calculating, test11)
+CTEST(calculate, test11)
 {
         ASSERT_EQUAL(118800, result(110000, 240));
 }
